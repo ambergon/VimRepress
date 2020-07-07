6b12092b3c1e2d6e88f75926ce58edeaac6060eb
ambergon <sc.foxgon@gmail.com>
2020-07-07 23:19:49 +0900

    [change]
    過去に無効化したcustom_span_classを使えるように

--- plugin/vimrepress.py
+++ plugin/vimrepress.py
@@ -426,12 +426,8 @@ class ContentStruct(object):
 
             struct["description"] = self.html_text = markdown.markdown(rawtext, \
                     extensions=['markdown.extensions.extra', \
-                    'markdown.extensions.nl2br'])
-                    #一時的に隔離
-                    #名前の変更か？
-                    #spanを使うか確かめる
-                    #, \
-                    #'custom_span_class'])
+                    'markdown.extensions.nl2br', \
+                    'mdx_custom_span_class'])
         else:
             struct["description"] = self.html_text = rawtext
 
